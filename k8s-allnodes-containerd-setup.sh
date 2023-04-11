5cfd2278ff6713d5b2e3c5c8f6c0d7ad2ff83591
Hamdini Cherif <ryadhamdini@gmail.com>
2023-04-11 16:22:28 +0200

    adding mark hold on kubelet

--- k8s-allnodes-containerd-setup.sh
+++ k8s-allnodes-containerd-setup.sh
@@ -67,4 +67,5 @@ sudo apt install apt-transport-https ca-certificates curl -y
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update
-sudo apt install -y kubelet
+sudo apt install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
