ef5737dfdd84792844c52bba27d78e9f1681f97f
Hamdini Cherif <ryadhamdini@gmail.com>
2023-04-12 10:05:43 +0200

    autocompletion for kubectl

---
+++ kubectl-autocompletion.sh
@@ -0,0 +1,9 @@
+#! /bin/bash -e 
+
+sudo apt install -y bash-completion
+
+source /usr/share/bash-completion/bash_completion
+
+source ~/.bashrc
+
+echo 'source <kubectl completion bash)' >>~/.bashrc
