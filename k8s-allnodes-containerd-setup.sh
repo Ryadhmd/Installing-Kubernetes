c84c7eb012213263a2b8d25e6d8bcbde46cd37ff
Cherif Hamdini <72081902+Ryadhmd@users.noreply.github.com>
2023-05-09 22:41:09 +0200

    Update k8s-allnodes-containerd-setup.sh

--- k8s-allnodes-containerd-setup.sh
+++ k8s-allnodes-containerd-setup.sh
@@ -67,5 +67,6 @@ sudo apt install apt-transport-https ca-certificates curl -y
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update
+# Version 1.27.1 
 sudo apt install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
